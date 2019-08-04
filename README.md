f2ecd98b0c7a8e6d6f928434bb70526b13ea049a
Aditya Widayanto <devjurnal.id@gmail.com>
2019-08-04 16:28:48 +0700

    add functions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-customWordpressFunctions
+# Custom Functions.php di wordpress untuk menampilkan beberapa fitur
+
+## menampilkan GIST pada halaman post
+## menampilkan featured image pada respon WP API v2
---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+
+// Put this code -> show gist github
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+class Gists_oEmbed {
+	public function __construct(){
+		add_action( 'init', array( $this, 'setup_handler' ) );
+	}
+	public function gist_result( $url ){
+		$url = $url[0];
+		if( !preg_match( '#\.js$#i', $url ) )
+			$url .= '.js';
+		return '<script src="' . $url . '"></script>';
+	}
+	public function setup_handler(){
+		wp_embed_register_handler( 'gist', '#https?://gist.github.com/.*#i', array( $this, 'gist_result' ) );
+	}
+}
+$gists_oembed = new Gists_oEmbed();
+
+// Put this code -> show featured image
+
+add_action('rest_api_init', 'register_rest_images' );
+
+function register_rest_images(){
+    register_rest_field( array('post'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
