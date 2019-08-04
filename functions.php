4f8bf53035381a244c284e80938ed92fbf4f9009
Aditya Widayanto <devjurnal.id@gmail.com>
2019-08-04 17:10:43 +0700

    change object name featured image

--- functions.php
+++ functions.php
@@ -24,12 +24,10 @@ class Gists_oEmbed {
 $gists_oembed = new Gists_oEmbed();
 
 // Put this code -> show featured image
-
 add_action('rest_api_init', 'register_rest_images' );
-
 function register_rest_images(){
     register_rest_field( array('post'),
-        'fimg_url',
+        'featured_img_url',
         array(
             'get_callback'    => 'get_rest_featured_image',
             'update_callback' => null,
@@ -37,7 +35,6 @@ function register_rest_images(){
         )
     );
 }
-
 function get_rest_featured_image( $object, $field_name, $request ) {
     if( $object['featured_media'] ){
         $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
